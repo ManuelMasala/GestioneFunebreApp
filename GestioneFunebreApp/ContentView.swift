c8fcc767a2a035ee9233fcf7dbafe3d0aee216e4
Litos245 <predatorino98@gmail.com>
2025-07-07 12:43:04 +0200

    Initial Commit

---
+++ GestioneFunebreApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GestioneFunebreApp
+//
+//  Created by Marco Lecca on 07/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GestioneFunebreApp/GestioneFunebreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestioneFunebreAppApp.swift
+//  GestioneFunebreApp
+//
+//  Created by Marco Lecca on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GestioneFunebreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
